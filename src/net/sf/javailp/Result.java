4d7bbd825eca98f6a290b9220ee9d01a5ac4683e
Martin Lukasiewycz <lukasiewycz@gmail.com>
2009-01-04 11:36:48 +0000

    Fixing wrong project name in license description
    git-svn-id: https://svn.code.sf.net/p/javailp/svn/trunk@16 2a5ecec7-8f1b-40c5-8731-7761254f6673

--- src/net/sf/javailp/Result.java
+++ src/net/sf/javailp/Result.java
@@ -4,13 +4,13 @@
  * Free Software Foundation, either version 3 of the License, or (at your
  * option) any later version.
  *
- * Opt4J is distributed in the hope that it will be useful, but WITHOUT
+ * Java ILP is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
  * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
  * License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with Opt4J. If not, see http://www.gnu.org/licenses/.
+ * License along with Java ILP. If not, see http://www.gnu.org/licenses/.
  */
 package net.sf.javailp;
 
